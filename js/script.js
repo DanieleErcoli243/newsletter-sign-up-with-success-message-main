45226c6ffe02089b0efbd57ffe95e6722c0ff6b5
danieleercoli243 <danielercoli@gmail.com>
2025-02-06 15:21:19 +0100

    working on validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('js ok');
+// recupero gli elementi dal DOM
+
+const form = document.querySelector('form');
+const field = document.querySelector('input');
+const messageField = document.getElementById('error-msg');
+const dismissBtn = document.getElementById('dismiss');
+
+// aggancio un ascoltatore di eventi al botton del form
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const valueField = field.value;
+
+    let errorMessage = '';
+
+    if(!valueField) errorMessage = 'e-mail is required';
+
+    const isEmailValid = /^\S+@\S+$/g
+
+    
+
+})
