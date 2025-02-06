5949577f43df997467c324ae0a43e9bd5d9d0cb3
danieleercoli243 <danielercoli@gmail.com>
2025-02-06 17:49:25 +0100

    It looks like everything is working

--- js/script.js
+++ js/script.js
@@ -4,20 +4,33 @@ const form = document.querySelector('form');
 const field = document.querySelector('input');
 const messageField = document.getElementById('error-msg');
 const dismissBtn = document.getElementById('dismiss');
-
+const overlay = document.querySelector('.overlay');
 // aggancio un ascoltatore di eventi al botton del form
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-
+    console.log('ao a fata');
     const valueField = field.value;
 
     let errorMessage = '';
-
-    if(!valueField) errorMessage = 'e-mail is required';
-
     const isEmailValid = /^\S+@\S+$/g
 
-    
+    if(!valueField) errorMessage = 'e-mail is required';
+    else if(!isEmailValid.test(valueField)) errorMessage = 'Please, enter a valid e-mail';
+
+    messageField.innerText= errorMessage;
+
+
+    if(!errorMessage) {
+        
+        overlay.classList.remove('d-none');
+        overlay.classList.add('d-flex');
+    }
+})
+
+dismissBtn.addEventListener('click', () => {
+    location.reload();
+    overlay.classList.remove('d-flex');
+    overlay.classList.add('d-none');
 
 })
